2267f18336cfd581ea2d07336afa61cd93704cb1
Ulhaq <ullhaq@hotmail.com>
2020-10-13 13:22:44 +0200

    password not null

--- src/main/resources/db/migration/V1__create_users_table.sql
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     name VARCHAR(255),
     username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255)
+    password VARCHAR(255) NOT NULL
 );
 
 SELECT setval('users_id_seq', 3, true);
