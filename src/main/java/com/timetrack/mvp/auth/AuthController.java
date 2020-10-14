cad6df8e85920e6dc189b468e5ac1c678fcf633d
Ulhaq <ullhaq@hotmail.com>
2020-10-14 20:12:38 +0200

    context-path added

--- src/main/java/com/timetrack/mvp/auth/AuthController.java
+++ src/main/java/com/timetrack/mvp/auth/AuthController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class AuthController {
     private AuthService authservise;
     
