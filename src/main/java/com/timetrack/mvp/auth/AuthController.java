b354655bab5003d1c82c0f71f16f1c70597e57ff
Ulhaq <ullhaq@hotmail.com>
2020-10-14 20:42:11 +0200

    redundant import removed

--- src/main/java/com/timetrack/mvp/auth/AuthController.java
+++ src/main/java/com/timetrack/mvp/auth/AuthController.java
@@ -11,7 +11,6 @@ import com.timetrack.mvp.users.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
